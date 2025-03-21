095dda5f088168ccc80d9d27659dfc46594da21a
Seth Miller <71085273+sm3136@users.noreply.github.com>
2025-03-21 12:01:10 -0400

    refactored code

--- src/index.ts
+++ src/index.ts
@@ -24,36 +24,36 @@ function identifyCallExpressions(file: SourceFile, exprName: string): CallExpres
 	});
 }
 
+function transform(program: ts.Program, context: ts.TransformationContext, sourceFile: ts.SourceFile): ts.SourceFile {
+	// Create a new ts-morph project with the compiler options from the current program
+	const project = new Project({
+		compilerOptions: program.getCompilerOptions(),
+		skipAddingFilesFromTsConfig: true,
+	});
+
+	// Create (or update) a ts-morph source file using the existing file text.
+	const file = project.createSourceFile(sourceFile.fileName, sourceFile.getFullText(), { overwrite: true });
+
+	// Apply transformations using ts-morph
+	removeImportDeclaration(file, join(__dirname, "..", "index.d.ts"));
+
+	// Identify and transform call expressions
+	const callExpressions = identifyCallExpressions(file, "$stamp");
+	callExpressions.forEach((call) => {
+		const args = call.getTypeArguments();
+		if (args.length !== 1) return;
+
+		const arg = args[0];
+		const ast = generateIIFEExpressionFromType(arg.getType());
+
+		call.transform(() => ast);
+	});
+
+	return file.compilerNode;
+}
+
 export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
-	return (context: ts.TransformationContext) => {
-		return (sourceFile: ts.SourceFile) => {
-			// Create a new ts-morph project with the compiler options from the current program
-			const project = new Project({
-				compilerOptions: program.getCompilerOptions(),
-				skipAddingFilesFromTsConfig: true,
-			});
-
-			// Create (or update) a ts-morph source file using the existing file text.
-			const file = project.createSourceFile(sourceFile.fileName, sourceFile.getFullText(), { overwrite: true });
-
-			// Apply transformations using ts-morph
-			removeImportDeclaration(file, join(__dirname, "..", "index.d.ts"));
-
-			// Identify and transform call expressions
-			const callExpressions = identifyCallExpressions(file, "$stamp");
-			callExpressions.forEach((call) => {
-				const args = call.getTypeArguments();
-				const firstArg = args[0];
-				if (!firstArg) {
-					console.error("No arguments found in the call expression.");
-					return;
-				}
-
-				const ast = generateIIFEExpressionFromType(firstArg.getType());
-				call.transform((node) => ast);
-			});
-
-			return file.compilerNode;
-		};
+	return (context: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+		return transform(program, context, sourceFile);
 	};
 }
